4cebf8fcddbbca155f09cb77897b0ad8703054b4
mohanedSalaheldin <mohnsd1162002@gmail.com>
2024-07-09 16:30:01 +0300

    maxProfit and maxSubArray problems

--- Arrays/Arrays2.java
+++ Arrays/Arrays2.java
@@ -109,16 +109,74 @@ public class Arrays2 {
         return null;
     }
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-     }
-    public static void main(String[] args) {
-        int[] arr = {2,7,11,15};
-        int[] sol =  (arr, 9);
-        for (int i = 0; i < sol.length; i++) {
-            System.out.println(sol[i]);
+        int first =0;
+        int mid =0;
+        int last  = nums.length-1;
+        while (mid<= last) {
+            if (nums[mid] == 0) {
+                int temp = nums[first];
+                nums[first] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                first++;
+            }else if (nums[mid] == 2) {
+                int temp = nums[last];
+                nums[last] = nums[mid];
+                nums[mid] = temp;
+                last--;
+            }else{
+                mid++;
+            }
         }
+        
+     }
 
-        System.out.println();
-        // System.out.println("Hello World");
+     public static int maxSubArray(int[] nums) {
+        int maxi = 0; 
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum>maxi) {
+                maxi = sum;
+            }
+            if (sum<0) {
+                sum =0;
+            }
+        }  
+        return maxi;
+     }
+
+     public int maxProfit(int[] prices) {
+        int prof = 0;
+        int pay = prices[0];
+        for(int i =1; i<prices.length; i++){
+            if(prices[i]< pay) pay = prices[i];
+            else if(prices[i] - pay > prof) prof = prices[i] - pay;
+        }
+        return prof;
+    }
+    public int maxSubArray(int[] nums) {
+        // if(nums.length==1) return nums[0];
+        int maxi = nums[0]; 
+        int sum = 0;
+        for(int i =0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>maxi) maxi = sum;
+            if(sum<0) sum = 0;
+        }
+        return maxi;
+    }
+    public static void main(String[] args) {
+        // int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] prices = {7,1,5,3,6,4};
+        int sol = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i; j < prices.length; j++) {
+                int sum = prices[j] - prices[i];
+                sol = Math.max(sol, sum);
+            }
+        }
+        System.out.println(sol);
+        
     }
 }
